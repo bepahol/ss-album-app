e13bb7a2da4180fdbddefdb12fa049f53ddf2217
Benjamin Holst <bepahol@gmail.com>
2020-09-18 09:20:17 -0600

    centered search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,6 +3,7 @@ import { Row, Col, Button } from "reactstrap";
 import AlbumSelectors from "selectors/AlbumSelectors";
 import { connect } from "react-redux";
 import Link from "next/link";
+import styles from "assets/styles/Search.module.css";
 
 class SearchBar extends React.Component {
   state = {
@@ -39,44 +40,38 @@ class SearchBar extends React.Component {
 
     return (
       <>
-        <div className="wrapper login_page">
-          <div className="main-panel" ref="mainPanel">
-            <Row>
-              <Col xs={12} md={12}>
-                <div className="container-fluid">
-                  <div className="login-wrapper row">
-                    <div id="login" className="login loginpage offset-xl-4 offset-lg-3 offset-md-3 offset-0 col-12 col-md-6 col-xl-4">
-                      {/* <form name="loginform" id="loginform" onSubmit={(event) => this.handleSearch(event)}> */}
-                      {/* using div here so elements are all on one line */}
-                      <label htmlFor="user_login">
-                        Search for your fav artist: &nbsp;
-                        <input
-                          type="text"
-                          name="artist"
-                          id="user_login"
-                          className="input"
-                          value={this.state.artist}
-                          size="20"
-                          onChange={(event) => this.handleChange(event)}
-                          onKeyDown={(event) => this.handleKeyDown(event)}
-                          disabled={fetching ? true : false}
-                        />
-                      </label>
-                      &nbsp;
-                      {/* <Link href={`/album/${this.state.artist}`}> */}
-                      <a href={`/album/${this.state.artist}`}>
-                        <Button color="secondary" disabled={!this.state.artist}>
-                          {fetching ? "Searching..." : "Search"}
-                        </Button>
-                      </a>
-                      {/* </Link> */}
-                      {/* <input type="submit" name="wp-submit" id="wp-submit" className="" value={fetching ? "Searching..." : "Search"} /> */}
-                      {/* </form> */}
-                    </div>
-                  </div>
-                </div>
-              </Col>
-            </Row>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className={styles.search}>
+                {/* <form name="loginform" id="loginform" onSubmit={(event) => this.handleSearch(event)}> */}
+                {/* using div here so elements are all on one line */}
+                <label htmlFor="artist">
+                  Search for your fav artist: &nbsp;
+                  <input
+                    type="text"
+                    name="artist"
+                    id="artist"
+                    className="input"
+                    value={this.state.artist}
+                    size="20"
+                    onChange={(event) => this.handleChange(event)}
+                    onKeyDown={(event) => this.handleKeyDown(event)}
+                    disabled={fetching ? true : false}
+                  />
+                </label>
+                &nbsp;
+                {/* <Link href={`/album/${this.state.artist}`}> */}
+                <a href={`/album/${this.state.artist}`}>
+                  <Button color="primary" disabled={!this.state.artist}>
+                    {fetching ? "Searching..." : "Search"}
+                  </Button>
+                </a>
+                {/* </Link> */}
+                {/* <input type="submit" name="wp-submit" id="wp-submit" className="" value={fetching ? "Searching..." : "Search"} /> */}
+                {/* </form> */}
+              </div>
+            </div>
           </div>
         </div>
       </>
